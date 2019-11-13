42f704600951687c8a3d300e98e6124428afcd76
MohammedSultanAlenazi <mohammedsultan@Mohammeds-MacBook-Pro.local>
2019-11-13 14:04:52 +0300

    game instructions

---
+++ memory_game/main.js
@@ -0,0 +1,10 @@
+console.log("Up and running!");
+var playerone ="KJ";
+var playertwo="LJ";
+var playerthree="KJ";
+var playerfour="LJ";
+console.log("User flipped " + playerone);
+console.log("User flipped " + playerfour);
+// console.log("User flipped " + cardOne);
+// console.log("User flipped " + cardOne);
+
